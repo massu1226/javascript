07b199aa4e9e69bf06e5485a50e6e589933609c9
massu1226 <121721453+massu1226@users.noreply.github.com>
2023-02-24 20:26:20 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+let i = Number
+ for (let i = 1; i < 51; i++){
+  if(i % 10 == 0){
+   console.log('今'+( i )+'回ループしました');
+    if(i % 4 == 0){
+      console.log('４で割れる数です。'+'['+(i)+']');
+    }
+  }else if(i % 4 == 0){
+   console.log('４で割れる数です。'+'['+(i)+']');
+  }else {
+    console.log(i);
+  } 
+  if(i === 50){
+  alert('50回カウントが終わりました');  
+  }
+}
